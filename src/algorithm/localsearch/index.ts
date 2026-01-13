030ccb59253e069c41a011c392671f553961446f
dangminh214 <dang_minh.nguyen@stud.tu-darmstadt.de>
2026-01-14 00:07:26 +0100

    tmp save

--- src/algorithm/localsearch/index.ts
+++ src/algorithm/localsearch/index.ts
@@ -1,12 +1,19 @@
 export interface Neighborhood {}
 
-export interface Geometrybased {}
+// do the Geometrybased first
+export interface Geometrybased { }
+
+
 export interface Rulebased {}
 export interface PartialOververlap {}
 
+export interface State {
+    isCompleted(): boolean;
+}
 
 export class LocalSearchSolver <
     E extends Neighborhood,
+    S extends State
 > {
-
+  neigborhoodType:
 }
