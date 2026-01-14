b79381ae3ea95fcaa959e359813ef0570c6d684f
Minh Nguyen <dangminh214@gmail.com>
2026-01-14 23:44:54 +0100

    refactor

--- src/algorithm/greedy/strategy.ts
+++ src/algorithm/greedy/strategy.ts
@@ -2,34 +2,34 @@ import type { GreedyExtender } from ".";
 import { Box } from "../box";
 import type { Rectangle } from "../rectangle";
 import type { PackingSolution } from "../solution";
-import type { PuttingStrategy } from "./putting";
+import type { PackingStrategy } from "./packing";
 
-export class FirstFitPlacer implements GreedyExtender<Rectangle, PackingSolution> {
-    private boxSize: number;
-    private puttingStrategy: PuttingStrategy;
+export class FirstFitPlacer implements GreedyExtender<
+  Rectangle,
+  PackingSolution
+> {
+  private boxSize: number;
+  private packingStrategy: PackingStrategy;
 
-    constructor(boxSize: number, puttingStrategy: PuttingStrategy) {
-        this.boxSize = boxSize;
-        this.puttingStrategy = puttingStrategy;
-    }
+  constructor(boxSize: number, packingStrategy: PackingStrategy) {
+    this.boxSize = boxSize;
+    this.packingStrategy = packingStrategy;
+  }
 
-    extend(
-        solution: PackingSolution,
-        rectangle: Rectangle
-    ): PackingSolution {
-        // Try to put boxes of the solution into a rectangle
-        for (const box of solution.boxes) {
-            if (this.puttingStrategy.tryPut(rectangle, box)) {
-                return solution;
-            }
-        }
-
-        // Creating new box
-        const newBox = new Box(this.boxSize);
-        rectangle.setPosition(0, 0);
-        newBox.addRect(rectangle);
-        solution.boxes.push(newBox);
-
+  extend(solution: PackingSolution, rectangle: Rectangle): PackingSolution {
+    // Try to put boxes of the solution into a rectangle
+    for (const box of solution.boxes) {
+      if (this.packingStrategy.tryPut(rectangle, box)) {
         return solution;
+      }
     }
+
+    // Creating new box
+    const newBox = new Box(this.boxSize);
+    rectangle.setPosition(0, 0);
+    newBox.addRect(rectangle);
+    solution.boxes.push(newBox);
+
+    return solution;
+  }
 }
