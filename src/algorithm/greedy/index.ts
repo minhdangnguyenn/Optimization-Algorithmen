089643ecf7028e7962ea07d688351f6141672866
Minh Nguyen <dangminh214@gmail.com>
2026-01-14 18:41:16 +0100

    adjust line space

--- src/algorithm/greedy/index.ts
+++ src/algorithm/greedy/index.ts
@@ -1,43 +1,37 @@
 export interface GreedyElement {}
 
 export interface GreedyState {
-    isComplete(): boolean;
+  isComplete(): boolean;
 }
 
 export interface OrderingStrategy<E extends GreedyElement> {
-    order(elements: readonly E[]): readonly E[];
+  order(elements: readonly E[]): readonly E[];
 }
 
-export interface GreedyExtender<E extends GreedyElement, S extends GreedyState> {
-    extend(state: S, element: E): S;
-}
-
-export class GreedySolver<
-    E extends GreedyElement,
-    S extends GreedyState
+export interface GreedyExtender<
+  E extends GreedyElement,
+  S extends GreedyState,
 > {
-    ordering: OrderingStrategy<E>;
-    extender: GreedyExtender<E, S>;
-
-    constructor(ordering: OrderingStrategy<E>, extender: GreedyExtender<E, S>) {
-        this.ordering = ordering;
-        this.extender = extender;
-    }
-
-    solve(
-        initialState: S,
-        elements: readonly E[],
-    ): S {
-
-        let state = initialState;
-        const orderedElements = this.ordering.order(elements); 
-        for (const element of orderedElements) {
-            if (state.isComplete()) break;
-            state = this.extender.extend(state, element);
-        }
-
-        return state;
-    }
+  extend(state: S, element: E): S;
 }
 
+export class GreedySolver<E extends GreedyElement, S extends GreedyState> {
+  ordering: OrderingStrategy<E>;
+  extender: GreedyExtender<E, S>;
 
+  constructor(ordering: OrderingStrategy<E>, extender: GreedyExtender<E, S>) {
+    this.ordering = ordering;
+    this.extender = extender;
+  }
+
+  solve(initialState: S, elements: readonly E[]): S {
+    let state = initialState;
+    const orderedElements = this.ordering.order(elements);
+    for (const element of orderedElements) {
+      if (state.isComplete()) break;
+      state = this.extender.extend(state, element);
+    }
+
+    return state;
+  }
+}
