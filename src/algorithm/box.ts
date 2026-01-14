78ef49a812679358f17a9364d714508bf9c224b7
Minh Nguyen <dangminh214@gmail.com>
2026-01-14 23:39:11 +0100

    refactor

--- src/algorithm/box.ts
+++ src/algorithm/box.ts
@@ -1,36 +1,34 @@
 import type { Rectangle } from "./rectangle";
 
 export class Box {
-    readonly size: number;
-    rectangles: Rectangle[];
-    utilization: number;
-    
-    get area(): number {
-        return this.size * this.size;
-    }
-    
-    constructor(size: number) {
-        this.size = size;
-        this.rectangles = [];
-        this.utilization = 0;
-    }
+  readonly length: number;
+  rectangles: Rectangle[];
+  utilization: number;
 
-    addRect(rectangle: Rectangle): void {
-        this.rectangles.push(rectangle);
-        const totalArea = this.rectangles.reduce((sum, rect) => sum + rect.area, 0);
-        this.utilization = (totalArea / this.area) * 100;
-    }
+  get area(): number {
+    return this.length * this.length;
+  }
 
-    isOverlapped(rect1: Rectangle, rect2: Rectangle): boolean {
-        const {x: x1, y: y1} = rect1.position;
-        const {x: x2, y: y2} = rect2.position;
-        return !(
-            x1! + rect1.width <= x2! ||
-            x2! + rect2.width <= x1! ||
-            y1! + rect1.height <= y2! ||
-            y2! + rect2.height <= y1!
-        );
-    }
+  constructor(size: number) {
+    this.length = size;
+    this.rectangles = [];
+    this.utilization = 0;
+  }
 
+  addRect(rectangle: Rectangle): void {
+    this.rectangles.push(rectangle);
+    const totalArea = this.rectangles.reduce((sum, rect) => sum + rect.area, 0);
+    this.utilization = (totalArea / this.area) * 100;
+  }
 
+  isOverlapped(rect1: Rectangle, rect2: Rectangle): boolean {
+    const { x: x1, y: y1 } = rect1.position;
+    const { x: x2, y: y2 } = rect2.position;
+    return !(
+      x1! + rect1.width <= x2! ||
+      x2! + rect2.width <= x1! ||
+      y1! + rect1.height <= y2! ||
+      y2! + rect2.height <= y1!
+    );
+  }
 }
